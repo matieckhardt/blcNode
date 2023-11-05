f4bb6df4d74e6dc2c0d5cae664896139f05a754b
matieckhardt <matias.eckhardt@gmail.com>
2023-11-05 12:45:48 -0300

    team fix

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path = require('path');
-const port = 3010;
+const path = require("path");
+const morgan = require("morgan"); // Import morgan
+const port = process.env.PORT || 3010; // Fixed the port variable
 
-app.use(express.urlencoded({
-  extended: false
-}));
+// Morgan middleware
+app.use(morgan("dev"));
+
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./routes/index'));
+// Import routes
+app.use(require("./routes/index"));
 
-app.listen(3010 || port, () => {
-  console.log('server on port: ', port)
-})
+app.listen(port, () => {
+  console.log(`server on port: ${port}`);
+});
--- public/js/index.js
+++ public/js/index.js
@@ -1,58 +1,66 @@
-$('#home-carrusel').slick({
-  arrows: false,
-  dots: true,
-  autoplay: true,
-  draggable: true,
-  fade: true,
-  lazyLoad: 'progressive',
-  infinite: true,
-  autoplaySpeed: 4000,
-  speed: 2000,
-}).slick("slickPause");
+$("#home-carrusel")
+  .slick({
+    arrows: false,
+    dots: true,
+    autoplay: true,
+    draggable: true,
+    fade: true,
+    lazyLoad: "progressive",
+    infinite: true,
+    autoplaySpeed: 4000,
+    speed: 2000,
+  })
+  .slick("slickPause");
 let initialDelay = 5000;
 setTimeout(function () {
-  $('#home-carrusel').slick("slickPlay");
+  $("#home-carrusel").slick("slickPlay");
 }, initialDelay);
 
-$('#alianzas-carrusel').slick({
+$("#alianzas-carrusel").slick({
   arrows: true,
   autoplay: true,
   draggable: true,
   fade: true,
-  lazyLoad: 'progressive',
+  lazyLoad: "progressive",
   infinite: true,
   autoplayspeed: 3000,
-  prevArrow: '<div class="slick-prev slick-arrow"><i class=" fas fa-chevron-left"></i></div>',
-  nextArrow: '<div class="slick-next slick-arrow"><i class=" fas fa-chevron-right"></i></div>',
-  responsive: [{
-    breakpoint: 768,
-    settings: {
-      fade: false,
-    }
-  }],
+  prevArrow:
+    '<div class="slick-prev slick-arrow"><i class=" fas fa-chevron-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next slick-arrow"><i class=" fas fa-chevron-right"></i></div>',
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        fade: false,
+      },
+    },
+  ],
 });
 
-$('#clientes-carrusel').slick({
+$("#clientes-carrusel").slick({
   dots: true,
   autoplay: true,
   draggable: true,
   fade: true,
-  lazyLoad: 'progressive',
+  lazyLoad: "progressive",
   infinite: true,
   autoplayspeed: 3000,
   slidesPerRow: 8,
   rows: 2,
-  responsive: [{
-    breakpoint: 478,
-    settings: {
-      slidesPerRow: 4,
-      fade: false,
-      rows: 4,
-    }
-  }]
+  responsive: [
+    {
+      breakpoint: 478,
+      settings: {
+        slidesPerRow: 4,
+        fade: false,
+        rows: 4,
+      },
+    },
+  ],
 });
 
-$('#testimonios-carrusel').slick({
+$("#testimonios-carrusel").slick({
   arrows: true,
   autoplay: false,
   draggable: true,
@@ -60,50 +68,55 @@ $('#testimonios-carrusel').slick({
   infinite: true,
   autoplayspeed: 3000,
   centerMode: true,
-  centerPadding: '100px',
+  centerPadding: "100px",
   slidesToShow: 1,
   dots: false,
-  prevArrow: '<div class="slick-prev slick-arrow"><i class=" fas fa-chevron-left"></i></div>',
-  nextArrow: '<div class="slick-next slick-arrow"><i class=" fas fa-chevron-right"></i></div>',
-  responsive: [{
-    breakpoint: 1600,
-    settings: {
-      centerPadding: '165px',
-    }
-  }]
+  prevArrow:
+    '<div class="slick-prev slick-arrow"><i class=" fas fa-chevron-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next slick-arrow"><i class=" fas fa-chevron-right"></i></div>',
+  responsive: [
+    {
+      breakpoint: 1600,
+      settings: {
+        centerPadding: "165px",
+      },
+    },
+  ],
 });
 
-$('#casos-carrusel').slick({
+$("#casos-carrusel").slick({
   arrows: true,
   dots: false,
   autoplay: false,
   accesibility: true,
   draggable: true,
   fade: true,
-  lazyLoad: 'progressive',
+  lazyLoad: "progressive",
   infinite: true,
   speed: 2000,
-  prevArrow: '<div class="slick-prev slick-arrow"><i class=" fas fa-chevron-left"></i></div>',
-  nextArrow: '<div class="slick-next slick-arrow"><i class=" fas fa-chevron-right"></i></div>',
+  prevArrow:
+    '<div class="slick-prev slick-arrow"><i class=" fas fa-chevron-left"></i></div>',
+  nextArrow:
+    '<div class="slick-next slick-arrow"><i class=" fas fa-chevron-right"></i></div>',
 });
 
-
-const animar = el => {
+const animar = (el) => {
   let imagen = el.firstElementChild.firstElementChild;
   let cuadrado = el.lastElementChild.lastElementChild.lastElementChild;
   imagen.classList.add("zoom");
   cuadrado.classList.add("achique");
-}
-const salir = el => {
+};
+const salir = (el) => {
   let imagen = el.firstElementChild.firstElementChild;
   let cuadrado = el.lastElementChild.lastElementChild.lastElementChild;
   imagen.classList.remove("zoom");
   cuadrado.classList.remove("achique");
 };
 
-$('.navbar-nav li a').on('click', function () {
-  if (!$(this).hasClass('dropdown-toggle')) {
-    $('.navbar-collapse').collapse('hide');
+$(".navbar-nav li a").on("click", function () {
+  if (!$(this).hasClass("dropdown-toggle")) {
+    $(".navbar-collapse").collapse("hide");
   }
 });
 
@@ -121,31 +134,31 @@ function enviarMensaje() {
 }
 
 function mensajeAJAX(nombre, email, asunto, mensaje, url) {
-  let urlCompleta = "https://bostoncelop.com.ar/send-mail"
+  let urlCompleta = "https://bostoncelop.com.ar/send-mail";
 
   let request = $.ajax({
     url: urlCompleta,
-    type: 'POST',
-    dataType: 'json',
+    type: "POST",
+    dataType: "json",
     data: {
       nombre: nombre,
       email: email,
       asunto: asunto,
       mensaje: mensaje,
       url: url,
-    }
-  })
+    },
+  });
   request.done(function (response) {
     if (response.success) {
       document.getElementById("form-contacto").reset();
-      $('#modalExito').modal('show');
+      $("#modalExito").modal("show");
     } else {
       document.getElementById("form-contacto").reset();
-      $('#modalFail').modal('show');
+      $("#modalFail").modal("show");
     }
-  })
+  });
   request.fail(function (response) {
     document.getElementById("form-contacto").reset();
-    $('#modalFail').modal('show');
-  })
+    $("#modalFail").modal("show");
+  });
 }
