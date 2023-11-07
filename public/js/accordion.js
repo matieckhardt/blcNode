91c604d929db97e0991e0eb90d3a2eded0302956
matieckhardt <matias.eckhardt@gmail.com>
2023-11-06 21:03:34 -0300

    last changes

--- public/js/accordion.js
+++ public/js/accordion.js
@@ -4,19 +4,19 @@ document.addEventListener("DOMContentLoaded", function () {
       title: "CAPACITACIÓN EN IDIOMAS",
       description: ` <strong>CURSOS REGULARES </strong><br />
 
-Con énfasis en la comunicación espontánea efectiva dentro de un marco curricular <br />
+Con énfasis en la comunicación espontánea efectiva dentro de un marco curricular. <br />
 
 <strong>PROGRAMAS CUSTOMIZADOS SEGÚN EL ROL EN LA ORGANIZACIÓN </strong><br />
 
-Con un diseño a medida de las necesidades, desafíos y tareas de los participantes <br />
+Con un diseño a medida de las necesidades, desafíos y tareas de los participantes. <br />
 
 <strong>CURSOS INTENSIVOS </strong><br />
 
-Para procesos de capacitación con intensa carga horaria <br />
+Para procesos de capacitación con intensa carga horaria. <br />
 
 <strong>CLASES DE CONVERSACIÓN </strong><br />
 
-Con dinámicas interactivas a través de técnicas de speeching, presentaciones in promptu y método PREP <br />
+Con dinámicas interactivas a través de técnicas de speeching, presentaciones in promptu y método PREP. <br />
 
 <strong>CAPACITACIONES TEMÁTICAS </strong><br />
 
@@ -24,19 +24,19 @@ Para adquirir vocabulario y habilidades clave para el desempeño laboral. <br />
 
 <strong>BLC ON-LINE </strong><br />
 
-Como complemento de nuestros programas, los participantes tienen acceso exclusivo a nuestra plataforma de e-learning con amplia variedad de actividades interactivas`,
+Como complemento de nuestros programas, los participantes tienen acceso exclusivo a nuestra plataforma de e-learning con amplia variedad de actividades interactivas.`,
       color: "#3D6F97", // Red
       zIndex: 5,
     },
     {
       title: "PREPARACIÓN PARA EXÁMENES INTERNACIONALES",
       description: `
-De inglés general o de negocios, de español y de portugués según nivel de idioma de los participantes`,
+De inglés general o de negocios, de español y de portugués según nivel de idioma de los participantes.`,
       color: "#DD1E97", // MediumAquamarine
       zIndex: 4,
     },
     {
-      title: "DIAGNÓSTICO DE DESEMPEÑO LINGUISTICO",
+      title: "DIAGNÓSTICO DE DESEMPEÑO LINGÜISTICO",
       description: `
 A través de una evaluación integral de nivel de lengua, medimos los conocimientos lingüísticos y la capacidad de un candidato para comunicarse en su potencial puesto de trabajo.`,
       color: "#8D5BB5", // MediumPurple
@@ -45,13 +45,13 @@ A través de una evaluación integral de nivel de lengua, medimos los conocimien
     {
       title: "SERVICIO DE TRADUCCIÓN Y SERVICIO DE INTERPRETACIÓN",
       description: `Servicio de traducción y servicio de Interpretación (simultánea, consecutiva, susurro y simultánea remota (ISR)).
-Con altos estándares de precisión, nuestro equipo lidera proyectos de traducción o interpretación en variadas industrias o áreas técnicas`,
+Con altos estándares de precisión, nuestro equipo lidera proyectos de traducción o interpretación en variadas industrias o áreas técnicas.`,
       color: "#83B58C", // Gold
       zIndex: 2,
     },
     {
-      title: "ASESORAMIENTO - AUDITORÍA ACADEMICA",
-      description: `Junto con nuestros clientes, instrumentamos programas de idiomas alineados con las necesidades de la empresa, diseñamos políticas de seguimiento y procesos de monitoreo y reporting`,
+      title: "ASESORAMIENTO - AUDITORÍA ACADÉMICA",
+      description: `Junto con nuestros clientes, instrumentamos programas de idiomas alineados con las necesidades de la empresa, diseñamos políticas de seguimiento y procesos de monitoreo y reporting.`,
       color: "#9AB5CA", // DeepSkyBlue
       zIndex: 1,
     },
